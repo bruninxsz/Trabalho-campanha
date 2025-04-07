f6d5370f59d2aa434ea1a7e98d8db85f5a24891f
Guilherme0309 <guiataidepires6@gmail.com>
2025-04-07 15:57:32 -0300

    initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+//AO INICIAR UM ARQUIVO JS SEMPRE DECLARE UMA VARIAVEL DE SUA BIBLIOTECA
+const express = require("express");
+const session = require("express-session");
+const sqlite3 = require("sqlite3");
+
+const app = express(); //Armazena as chamadas e propriedades da biblioteca EXPRESS
+
+const PORT = 8000;
+
+app.use('/static', express.static(__dirname + '/static'))
+
+app.set('view engine', 'ejs');
+
+app.get("/", (req, res) => {
+    console.log("GET /")
+    res.render("index");
+})
+
+app.get("/sobre", (req, res) => {
+    console.log("GET /sobre")
+    res.render("sobre");
+})
+
+app.listen(PORT, () => {
+    console.log(`Servidor sendo excexutado na porta ${PORT}`)
+    console.log(__dirname + "\\static")
+});
